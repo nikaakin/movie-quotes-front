4ceb5523fcb254d8041393d04325d659a4b308de
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-06-21 15:43:25 +0400

    feat: add mobile styles to page

--- pages/unauthorized.tsx
+++ pages/unauthorized.tsx
@@ -6,7 +6,7 @@ export default function Custom403() {
   const { t, onClick } = use403();
 
   return (
-    <div className='bg-lg-main w-screen h-screen flex justify-center items-center text-white'>
+    <div className='bg-lg-main w-screen h-screen flex justify-center items-center text-white px-10'>
       <div className='flex flex-col items-center'>
         <div className='relative w-80 h-60'>
           <img
@@ -20,10 +20,14 @@ export default function Custom403() {
             className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
           />
         </div>
-        <h1 className='mt-8 text-5xl font-bold'>{t('403.title')}</h1>
-        <p className='mt-4  text-2xl font-medium mb-12'>{t('403.text')}</p>
+        <h1 className='mt-8 sm:text-5xl text-2xl font-bold'>
+          {t('403.title')}
+        </h1>
+        <p className='mt-4 text-base  sm:text-2xl font-medium sm:mb-12 mb-8 text-center'>
+          {t('403.text')}
+        </p>
         <Button
-          classes='!text-xl px-4 py-3 '
+          classes='sm:!text-xl text-base px-7  sm:px-4 py-2 sm:py-3 '
           onClick={onClick}
           content={t('403.button')}
         />
