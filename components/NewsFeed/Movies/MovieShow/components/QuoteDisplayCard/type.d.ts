b34fae47608f167d65e06d844c550dd0ae66464e
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-02 17:33:47 +0400

    feat: add quote display card in movie show

---
+++ components/NewsFeed/Movies/MovieShow/components/QuoteDisplayCard/type.d.ts
@@ -0,0 +1,12 @@
+import { QuoteType } from '@/types';
+
+export type QuoteDisplayCardProps = {
+  quote: QuoteType;
+};
+
+export type useQuoteDisplayCardArgs = {
+  quote: {
+    en: string;
+    ka: string;
+  };
+};
