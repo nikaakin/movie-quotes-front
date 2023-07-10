efbab31466565dfaa269e748fb954692ac2d3d55
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-10 19:53:19 +0400

    refactor: chnage quote mutation modal properties

--- components/NewsFeed/Home/components/WriteQuoteButton/useWriteQuoteButton.ts
+++ components/NewsFeed/Home/components/WriteQuoteButton/useWriteQuoteButton.ts
@@ -1,20 +1,14 @@
-import { RootState, setCurrentModal } from '@/state';
+import { setCurrentModal } from '@/state';
 import { useTranslation } from 'next-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 export const useWriteQuoteButton = () => {
   const { t } = useTranslation(['common']);
-  const {
-    currentModal: { currentModal },
-  } = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
-  const toggleSearchBar = () => dispatch(setCurrentModal('add-quote'));
-  const onClose = () => dispatch(setCurrentModal(null));
+  const onAddQuote = () => dispatch(setCurrentModal('add-quote'));
 
   return {
     t,
-    toggleSearchBar,
-    onClose,
-    currentModal,
+    onAddQuote,
   };
 };
