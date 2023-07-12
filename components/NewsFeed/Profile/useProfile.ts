df2d43dd15af899f6848b2bff7c0caf7169bb891
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-13 02:58:32 +0400

    fix: update user data after editing

--- components/NewsFeed/Profile/useProfile.ts
+++ components/NewsFeed/Profile/useProfile.ts
@@ -53,6 +53,7 @@ export const useProfile = () => {
     mutationFn: edit,
     onSuccess: (data) => {
       queryClient.setQueryData(['user'], data.data.user);
+      queryClient.invalidateQueries(['user']);
       dispatch(setCurrentModal('edit-notification'));
       setTimeout(onClose, 2000);
     },
