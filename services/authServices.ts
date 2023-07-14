db1f589090498e3f864b09613d341af542f2f438
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-15 00:56:47 +0400

    refactor: add space between service functions

--- services/authServices.ts
+++ services/authServices.ts
@@ -11,15 +11,21 @@ import { SocketIoChannel } from 'laravel-echo/dist/channel';
 
 export const login = (data: loginSchemaType) =>
   axios().post('/api/login', data);
+
 export const logout = () => axios().get('/api/logout');
+
 export const register = (data: registrationSchemaType) =>
   axios().post('/api/register', data);
+
 export const edit = (data: registrationSchemaType) =>
   axios().patch('/api/update', data);
+
 export const forgotPassword = (data: forgotPasswordSchemaType) =>
   axios().post('/api/forgot-password', data);
+
 export const resetPassword = ({ token, email, password }: resetPasswordArgs) =>
   axios().post('/api/reset-password', { token, email, password });
+
 export const verifyEmail = (
   id: string,
   hash: string,
@@ -29,11 +35,14 @@ export const verifyEmail = (
   axios().get(`/api/email/verify/${id}/${hash}`, {
     params: { expires, signature },
   });
+
 export const googleLogin = (query: ParsedUrlQuery) =>
   axios().get('/api/auth/google/callback', { params: query });
 
 export const getCsrf = () => axios().get('/sanctum/csrf-cookie');
+
 export const isAuthenticated = () => axios().get('/api/user');
+
 export const broadcastAuth = (
   socketId: number,
   callback: AuthorizerCallback,
