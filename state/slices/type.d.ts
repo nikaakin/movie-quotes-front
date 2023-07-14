78db98b106318aa3fd5633239c9da4c0ddf51173
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-15 00:53:25 +0400

    fix: declare type of current modal

---
+++ state/slices/type.d.ts
@@ -0,0 +1,18 @@
+export type currentModalType =
+  | 'login'
+  | 'register'
+  | 'forgot-password'
+  | 'reset-password'
+  | 'account-activated'
+  | 'password-changed'
+  | 'link-expired'
+  | 'register-notification'
+  | 'edit-notification'
+  | 'forgot-password-notification'
+  | 'confirmation-notification'
+  | 'add-movie'
+  | 'add-quote'
+  | 'quote-view'
+  | 'edit-quote'
+  | 'edit-movie'
+  | null;
