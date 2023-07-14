ac7c8348379bb4fa2bb41d9607d1b115b5edcb9a
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-14 10:56:36 +0400

    fix: dont show landing page to logged in user

--- components/shared/Header/useHeader.ts
+++ components/shared/Header/useHeader.ts
@@ -1,8 +1,8 @@
+import { setCookie } from '@/helpers';
 import { useUserQuery } from '@/hooks';
 import { getCsrf, isAuthenticated, logout } from '@/services';
 import { setCurrentModal } from '@/state';
 import { setIsSearchBarOn } from '@/state';
-import { useQuery } from '@tanstack/react-query';
 import Echo from 'laravel-echo';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
@@ -10,6 +10,7 @@ import { useDispatch } from 'react-redux';
 
 export const useHeader = () => {
   const disaptch = useDispatch();
+  const { push } = useRouter();
   const { t } = useTranslation('common');
   const {
     query: { slug },
@@ -20,25 +21,23 @@ export const useHeader = () => {
     queryFn: isAuthenticated,
   });
 
-  const { refetch: logoutUser } = useQuery({
-    queryKey: ['user'],
+  const { refetch: logoutUser } = useUserQuery({
     queryFn: () => logout(),
-    onSuccess: () =>
-      (window as Window &
-        typeof globalThis & { Echo: Echo })!.Echo!.leaveChannel(
-        'notification.' + data?.id
-      ),
     enabled: false,
-    refetchOnWindowFocus: false,
+    isLogout: true,
   });
 
   const username = data?.username;
-  const { push } = useRouter();
   const onShowLogin = () => disaptch(setCurrentModal('login'));
   const onShowRegister = () => disaptch(setCurrentModal('register'));
   const onLogout = async () => {
     await getCsrf();
-    logoutUser().then(() => push('/'));
+    await logoutUser();
+    setCookie('user', 'win');
+    (window as Window & typeof globalThis & { Echo: Echo })!.Echo!.leaveChannel(
+      'notification.' + data?.id
+    );
+    push('/');
   };
   const onSearchBarClick = () => disaptch(setIsSearchBarOn(true));
 
