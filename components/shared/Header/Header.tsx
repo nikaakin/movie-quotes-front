669d7865e63676baec72b59388c02ebc77f74010
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-16 14:08:31 +0400

    fix: remoce redundant link

--- components/shared/Header/Header.tsx
+++ components/shared/Header/Header.tsx
@@ -22,14 +22,13 @@ export const Header = ({ shouldhavelinks = false }) => {
         shouldhavelinks ? 'px-8' : 'px-4'
       } ${username && 'bg-zinc-850 bg-opacity-80'}`}
     >
-      <Link
-        href='/'
-        className={`hidden sm:block ${!shouldhavelinks && '!block'}`}
+      <h1
+        className={`uppercase text-orange-250 text-base font-medium hidden sm:block ${
+          !shouldhavelinks && '!block'
+        }`}
       >
-        <h1 className='uppercase text-orange-250 text-base font-medium '>
-          movie quotes
-        </h1>
-      </Link>
+        movie quotes
+      </h1>
       <Burgerbar shouldShowLinks={shouldhavelinks} onLogout={onLogout} />
       <div className='flex flex-row justify-between gap-2 sm:gap-4'>
         {username ? (
