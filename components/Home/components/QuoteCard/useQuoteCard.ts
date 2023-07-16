574e2adffcf97cdc0ccd3e639811bad4869b7452
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-16 20:07:51 +0400

    refactor: remove unnecessary state update

--- components/Home/components/QuoteCard/useQuoteCard.ts
+++ components/Home/components/QuoteCard/useQuoteCard.ts
@@ -1,7 +1,6 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { useQuoteCardArgs } from './type';
 import { useComment, useLike } from '@/hooks';
-import { QuoteType } from '@/types';
 
 export const useQuoteCard = ({
   current_user_likes,
@@ -9,37 +8,9 @@ export const useQuoteCard = ({
   notifications,
   id,
 }: useQuoteCardArgs) => {
-  const queryClient = useQueryClient();
-
-  const onLikeUpdate = (liked: boolean) => {
-    queryClient.setQueryData<{ pages: { quotes: QuoteType[] }[] }>(
-      ['quotes'],
-      (prev) => {
-        if (!prev) return prev;
-        const updatedPages = prev.pages.map(({ quotes }) => ({
-          quotes: quotes.map((oldQuote) =>
-            oldQuote.id === id
-              ? {
-                  ...oldQuote,
-                  current_user_likes: liked ? 1 : 0,
-                  likes: liked ? oldQuote.likes + 1 : oldQuote.likes - 1,
-                }
-              : oldQuote
-          ),
-        }));
-        return {
-          ...prev,
-          pages: updatedPages,
-        };
-      }
-    );
-    queryClient.invalidateQueries(['quotes']);
-  };
-
   const { onLike, isLoading } = useLike({
     current_user_likes,
     likes,
-    onSuccess: onLikeUpdate,
   });
   const { comment, onComment, onCommentChange } = useComment({
     notifications,
