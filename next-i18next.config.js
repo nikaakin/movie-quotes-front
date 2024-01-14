8f366e9f3d0a3f983ee92843a97d1342876fdd6e
Nika Cuckiridze <nikacuckiridze47@gmail.com>
2024-01-14 06:52:38 +0400

    fix: locale path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ka'],
+    localePath:
+      typeof window === 'undefined'
+        ? require('path').resolve('./public/locales')
+        : '/locales',
   },
 };
